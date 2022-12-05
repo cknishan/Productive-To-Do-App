a63b6e67a8e2fc7bf9f9baf1d7edbbc2b8c73b0f
Nishan <nishan.chakma.nish@gmail.com>
2022-12-05 17:41:49 +1100

    create task done

--- js/createTasks.js
+++ js/createTasks.js
@@ -1,1 +1,45 @@
 "use strict"
+
+// reference to input and form
+const task = document.querySelector("#task-input")
+const taskForm = document.querySelector("#add-task")
+
+// reference to todo tasks section
+const todoTasks = document.querySelector("#todoTasks")
+ 
+taskForm.addEventListener('submit', (e)=> {
+    e.preventDefault()
+    
+    // if the form input is not empty or whitespaces
+    if (task.value.trim() != "" ){
+
+        // create new html
+        let newTask = `
+        <div class="task" >
+        <div>${task.value.trim()}</div>
+        <div class="grid place-items-center">                
+          <span>
+            <!-- empty box icon -->
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7 inline-block">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z" />
+            </svg>  
+            <!-- menu icon -->
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline-block">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>  
+            <!-- trashcan icon -->
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline-block">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+            </svg>
+        </span>
+        </div>
+        </div>
+        `
+        // add the html to the todoTasks section
+        todoTasks.innerHTML += newTask
+    }
+
+    task.value = ""
+
+
+})
--- js/responsiveMenu.js
+++ js/responsiveMenu.js
@@ -4,10 +4,14 @@ const burger = document.querySelector("#burger")
 const menu = document.querySelector("#menu")
 
 burger.addEventListener('click', ()=> {
-    if (menu.classList.contains('hidden')) {
-        menu.classList.remove('hidden')
+    if (menu.classList.contains("hidden")) {
+        menu.classList.remove("hidden")
+        menu.classList.remove("z-5")
+        menu.classList.remove("absolute")
     } else {
-        menu.classList.add('hidden')
+        menu.classList.add("hidden")
+        menu.classList.add("z-5")
+        menu.classList.add("absolute")
     }
     console.log("click")
 })

5cd3d2f58d0fa48c6a2e234f76748efe3cd5cc4f
Nishan <nishan.chakma.nish@gmail.com>
2022-12-07 15:28:18 +1100

    omitted check icon for simplicity

--- js/createTasks.js
+++ js/createTasks.js
@@ -23,10 +23,6 @@ taskForm.addEventListener('submit', (e)=> {
         <div>${task.value.trim()}</div>
         <div class="grid place-items-center">                
           <span>
-            <!-- empty box icon -->
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7 inline-block checkIcon">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z" />
-            </svg>  
             <!-- menu icon -->
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline-block menuIcon">
               <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -48,7 +44,6 @@ taskForm.addEventListener('submit', (e)=> {
     // everytime a new task is added 
     // the following reference and bindings needs to be updated as well
 
-    let checkIcon = document.querySelectorAll(".checkIcon")
     let menuIcon = document.querySelectorAll(".menuIcon")
     let trashIcon = document.querySelectorAll(".trashIcon")
 
@@ -60,56 +55,6 @@ taskForm.addEventListener('submit', (e)=> {
         })
     })
 
-    // complete task functionality
-    checkIcon.forEach((iconElement) => {
-      iconElement.addEventListener('click', (pointerEvent) => {
-
-        let taskElement = iconElement.parentElement.parentElement.parentElement
-        let todoText = taskElement.firstElementChild.textContent
-
-        // remove from todo
-        taskElement.remove()
-
-        // add to completed
-        completedTasks.innerHTML += `
-        <div class="task">
-          <div class="line-through">${todoText}</div>
-          <div class="grid place-items-center">                
-            <span>
-              <!-- check icon -->
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline-block checkIcon">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-              </svg>
-              
-              <!-- menu icon -->
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline-block menuIcon">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-              </svg>  
-              <!-- trashcan icon -->
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline-block trashIcon">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-              </svg>
-            </span>
-          </div>
-        </div>
-      </div>
-      `
-
-    // everytime a new task is added 
-    // the following reference and bindings needs to be updated as well
-    let menuIcon = document.querySelectorAll(".menuIcon")
-    let trashIcon = document.querySelectorAll(".trashIcon")
-
-      // trasch icon delete task functionality
-      trashIcon.forEach((iconElement) => {
-        iconElement.addEventListener('click', (pointerEvent) => {
-            let taskElement = iconElement.parentElement.parentElement.parentElement
-            taskElement.remove()
-        })
-      })
-      
-    })
-  })
 
 
 })
